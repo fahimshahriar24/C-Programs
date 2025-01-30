633de30a6f8f2d305cf00de817577042fd0fe777
Fahim Shahriar <170297987+fahimshahriar24@users.noreply.github.com>
2025-01-30 11:42:07 +0600

    changes

--- CodeWithHarry/Chapter 1/AreaCircle.c
+++ CodeWithHarry/Chapter 1/AreaCircle.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int radius;
     float area;
 
     printf("Enter the radius: ");
-    scanf("%d",&radius);
-    
-    area=3.1416*radius*radius;
+    scanf("%d", &radius);
 
-    printf("The area of the circle is: %f",area);
-    
+    area = 3.1416 * radius * radius;
+
+    printf("The area of the circle is: %f", area);
+
     return 0;
 }
